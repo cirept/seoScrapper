e420a9bd1c4b3fd0edd45c9255546ff5f53ae045
Eric Tanaka <cirept@users.noreply.github.com>
2017-04-28 07:18:28 -0700

    Add files via upload

---
+++ seoScrapper-meta.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name SEO Scrapper
+// @namespace www.cobaltgroup.com/
+// @version 1.0
+// @author Eric Tanaka
+// @include http:*
+// @include https:*
+// @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
+// @require https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
+// @require https://raw.githubusercontent.com/cirept/seoScrapper/master/main-release.js
+// @resource fontAwe https://use.fontawesome.com/3953f47d82.js
+// @resource font https://fonts.googleapis.com/css?family=Montserrat
+// @resource jqueryUI https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css
+// @run-at document-end
+// @grant GM_openInTab
+// @grant GM_setClipboard
+// @grant unsafeWindow
+// @grant GM_setValue
+// @grant GM_getValue
+// @grant GM_getResourceURL
+// @grant GM_info
+// @grant GM_listValues
+// @noframes
+// ==/UserScript==
